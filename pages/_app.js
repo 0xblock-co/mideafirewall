9f268575323059c0c57cfc4b6df64a19a61da287
Jemish Harsora <jemish.me@gmail.com>
2023-03-16 22:58:31 +0530

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/module-style.scss";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.min.js");
+  }, []);
+
+  return <Component {...pageProps} />;
 }

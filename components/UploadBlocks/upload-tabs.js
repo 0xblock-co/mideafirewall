860b48fa933982f66c97bdc486d4270694a7aca9
Dhara Vara <dhara.var@gmail.com>
2023-03-28 22:56:16 +0530

    upload page design

---
+++ components/UploadBlocks/upload-tabs.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Tab from 'react-bootstrap/Tab';
+import Form from 'react-bootstrap/Form';
+import Tabs from 'react-bootstrap/Tabs';
+import Image from "next/image";
+import { Button } from "react-bootstrap";
+export default function UploadTabs() {
+    return (
+        <div className="mdf__upload__tab bg-white mt-5 rounded ">
+            <Tabs
+                defaultActiveKey="web"
+                id="uncontrolled-tab-example"
+            >
+                <Tab eventKey="web" className="p-3" title="Web">
+                    <div className="text-center">
+                        <h1>Please upload your files </h1>
+                        <Image
+                            className="w-25 mt-5"
+                            layout="fill"
+                            src="/images/upload.png"
+                            alt="A globe icon with filter and text."
+                        />
+                        <Form.Group controlId="formFile" className="mt-4">
+                            <Form.Label>Default file input example</Form.Label>
+                            <Form.Control type="file" />
+                        </Form.Group>
+                        <Button type="button" variant="primary" className="mt-3 py-2 px-5">
+                            Upload
+                        </Button>
+                    </div>
+                </Tab>
+                <Tab eventKey="program" className="p-3" title="Program">
+                    When I have seen by Time's fell hand defac'd The rich-proud cost of outworn buried age; When sometime lofty towers I see down-raz'd, And brass eternal slave to mortal rage; When I have seen the hungry ocean gain Advantage on the kingdom of the shore, And the firm soil win of the watery main, Increasing store with loss, and loss with store; When I have seen such interchange of state, Or state itself confounded, to decay;
+                </Tab>
+                <Tab eventKey="amazon" className="p-3" title="Amazons3">
+                    When I have seen by Time's fell hand defac'd The rich-proud cost of outworn buried age; When sometime lofty towers I see down-raz'd, And brass eternal slave to mortal rage; When I have seen the hungry ocean gain Advantage on the kingdom of the shore, And the firm soil win of the watery main, Increasing store with loss, and loss with store; When I have seen such interchange of state, Or state itself confounded, to decay;
+                </Tab>
+            </Tabs>
+        </div>
+    );
+}

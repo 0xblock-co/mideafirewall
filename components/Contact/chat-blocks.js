0fb994cfb1f33eb0eae60adafe79e35bd26dc3c0
jemish-0xblock <jemish.harsora@0xblock.co>
2023-10-26 10:21:12 +0530

    fix build issue

--- components/Contact/chat-blocks.js
+++ components/Contact/chat-blocks.js
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import style from "./contact.module.scss";
+import { Button,  Col, Container, Row } from "react-bootstrap";
 import WhatsAppChat from "./UI/whatsapp";
 import getConfig from "next/config";
 import ContactWithSalesChat from "./UI/ChatWithSalesTeam";
@@ -85,52 +83,52 @@ export default function ContactChatBlock({ handleBookMeeting }) {
     </section>
   );
 
-  return (
-    <section className={style.mdf__contactus_chatcard}>
-      <Container>
-        <Row className="justify-content-center">
-          <Col md={4} lg={3} xxl={3} className="mb-3">
-            <WhatsAppChat
-              phone={publicRuntimeConfig.whatsAppContactNumber}
-              message="Hello there!"
-            />
-          </Col>
-          <Col md={4} lg={3} xxl={3}>
-            <ContactWithSalesChat />
-          </Col>
-          <Col md={4} lg={3} xxl={3}>
-            <Card className={`${style.mdf__chatcard} mb-3`}>
-              <Image
-                className={style.chat__icons}
-                layout="fill"
-                src="/images/Play.png"
-                alt="A globe icon with filter and text."
-              />
-              <h6 className="my-4">Get a product demo</h6>
-              <Button
-                variant="primary"
-                onClick={() => router.push("/network-blog")}
-              >
-                Get a demo
-              </Button>
-            </Card>
-          </Col>
-          {/* <Col md={4} lg={3} xxl={3}>
-            <Card className={`${style.mdf__chatcard} mb-3`}>
-              <Image
-                className={style.chat__icons}
-                layout="fill"
-                src="/images/calendar.png"
-                alt="A globe icon with filter and text."
-              />
-              <h6 className="my-4">Book a Meeting</h6>
-              <Button variant="primary" onClick={() => handleBookMeeting(true)}>
-                Book
-              </Button>
-            </Card>
-          </Col> */}
-        </Row>
-      </Container>
-    </section>
-  );
+  // return (
+  //   <section className={style.mdf__contactus_chatcard}>
+  //     <Container>
+  //       <Row className="justify-content-center">
+  //         <Col md={4} lg={3} xxl={3} className="mb-3">
+  //           <WhatsAppChat
+  //             phone={publicRuntimeConfig.whatsAppContactNumber}
+  //             message="Hello there!"
+  //           />
+  //         </Col>
+  //         <Col md={4} lg={3} xxl={3}>
+  //           <ContactWithSalesChat />
+  //         </Col>
+  //         <Col md={4} lg={3} xxl={3}>
+  //           <Card className={`${style.mdf__chatcard} mb-3`}>
+  //             <Image
+  //               className={style.chat__icons}
+  //               layout="fill"
+  //               src="/images/Play.png"
+  //               alt="A globe icon with filter and text."
+  //             />
+  //             <h6 className="my-4">Get a product demo</h6>
+  //             <Button
+  //               variant="primary"
+  //               onClick={() => router.push("/network-blog")}
+  //             >
+  //               Get a demo
+  //             </Button>
+  //           </Card>
+  //         </Col>
+  //         {/* <Col md={4} lg={3} xxl={3}>
+  //           <Card className={`${style.mdf__chatcard} mb-3`}>
+  //             <Image
+  //               className={style.chat__icons}
+  //               layout="fill"
+  //               src="/images/calendar.png"
+  //               alt="A globe icon with filter and text."
+  //             />
+  //             <h6 className="my-4">Book a Meeting</h6>
+  //             <Button variant="primary" onClick={() => handleBookMeeting(true)}>
+  //               Book
+  //             </Button>
+  //           </Card>
+  //         </Col> */}
+  //       </Row>
+  //     </Container>
+  //   </section>
+  // );
 }
